8076c187448a7f11b98ae4cf121657f2f9759a9b
SergioGaitan27 <sergiogaitan203@gmail.com>
2023-11-14 19:27:48 -0600

    asyncAwait

---
+++ asyncAwait.js
@@ -0,0 +1,40 @@
+async function hola(nombre){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Hola ' + nombre);
+            resolve(nombre);
+        }, 1000);
+    });
+}
+
+async function adios(nombre){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Adios ' + nombre);
+            resolve();
+        }, 1000);
+    });
+}
+
+async function hablar(nombre){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('bla, bla, bla');
+            resolve(nombre);
+        }, 1000);
+    });
+}
+
+async function main(){
+    let nombre = await hola('Sergio');
+    await hablar();
+    await hablar();
+    await hablar();
+    await hablar();
+    await adios(nombre);
+    console.log('Termina el proceso');
+
+}
+
+console.log('Inicia el proceso...');
+main();
