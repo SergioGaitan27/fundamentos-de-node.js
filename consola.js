2af565ccfcf3ec2ac26109afbacd9075283e5600
SergioGaitan27 <sergiogaitan203@gmail.com>
2023-11-15 13:52:31 -0600

    consola

---
+++ consola.js
@@ -0,0 +1,9 @@
+console.group('Incio de conversación');
+console.log('Hola');
+console.log('Adios');
+console.count('veces: ')
+console.count('veces: ')
+console.count('veces: ')
+console.count('veces: ')
+console.count('veces: ')
+console.groupEnd('Termina la conversación');
