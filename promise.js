9f2eaf0490e549ad54da7f1d9e0b190741ece6ca
SergioGaitan27 <sergiogaitan203@gmail.com>
2023-11-14 18:53:11 -0600

    Promesas

---
+++ promise.js
@@ -0,0 +1,36 @@
+function hola(nombre){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Hola ' + nombre);
+            resolve(nombre);
+        }, 1000);
+    });
+}
+
+function adios(nombre){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Adios ' + nombre);
+            resolve();
+        }, 1000);
+    });
+}
+
+function hablar(nombre){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('bla, bla, bla');
+            resolve(nombre);
+        }, 1000);
+    });
+}
+
+console.log('Inicinado proceso...')
+hola('Sergio')
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(adios)
+    .then(() => {
+        console.log('Terminando proceso...')
+    });
